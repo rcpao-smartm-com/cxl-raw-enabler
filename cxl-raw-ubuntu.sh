77f4df353561cd0c6afed2caa1d861a275d96576
Roger C. Pao <roger.pao@smartm.com>
2024-04-23 17:55:50 -0700

    apt-get <- apt

--- cxl-raw-ubuntu.sh
+++ cxl-raw-ubuntu.sh
@@ -62,7 +62,7 @@ gcc --version
 
 
 uname -r
-apt source linux-image-unsigned-${UNAME_R}
+apt-get source linux-image-unsigned-${UNAME_R}
 RETVAL=$? # DBG: non-zero will use git clone
 if [ ${RETVAL} -eq 0 ]; then
   cd linux-hwe-${UNAME_R_2}-${UNAME_R_3} # "linux-hwe-6.5-6.5.0"
